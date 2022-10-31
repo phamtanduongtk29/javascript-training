e7550c01499ac57c3d5143dfe59fff3b9c0639c0
Duong <phamtanduongtk@gmail.com>
2022-10-31 10:40:27 +0700

    #6 AddEvent button update and handle update student

--- practices/practice-one/src/scripts/views/student-item.view.js
+++ practices/practice-one/src/scripts/views/student-item.view.js
@@ -1,5 +1,6 @@
-import { querySelector } from '../helpers/utils.js';
+import { querySelector, querySelectorAll } from '../helpers/utils.js';
 import Controller from '../controllers/student.controller.js';
+import Student from '../models/students.model.js';
 
 export default class StudentItemView {
     #controler;
@@ -7,9 +8,22 @@ export default class StudentItemView {
     #overlay;
     #formUpdate;
     #formAdd;
+
     #id;
     #name;
     #image;
+
+    // get field
+    #nameUpdate;
+    #codeUpdate;
+    #dateOfBirthUpdate;
+    #classCodeUpdate;
+    #genderUpdate;
+
+    // get button
+    #btnUpdate;
+    #btnDelete;
+    #icons;
     constructor(id, name, image) {
         this.#controler = new Controller();
         this.#overlay = querySelector('.overlay');
@@ -17,6 +31,17 @@ export default class StudentItemView {
         this.#formAdd = querySelector('.form-add-wrapper');
         this.#liElement = document.createElement('li');
         this.#liElement.classList.add('student-item');
+        this.#codeUpdate = this.#formUpdate.querySelector('#update-code');
+        this.#nameUpdate = this.#formUpdate.querySelector('#update-name');
+        this.#dateOfBirthUpdate = this.#formUpdate.querySelector(
+            '#update-date-of-birth'
+        );
+        this.#classCodeUpdate =
+            this.#formUpdate.querySelector('#update-class-code');
+        this.#genderUpdate = this.#formUpdate.querySelector('#update-gender');
+        this.#btnUpdate = querySelector('#update');
+        this.#btnDelete = querySelector('#delete');
+        this.#icons = querySelectorAll('.information-item-update ion-icon');
         this.#id = id;
         this.#name = name;
         this.#image =
@@ -40,6 +65,58 @@ export default class StudentItemView {
         this.#formAdd.style.display = 'none';
     }
 
+    #addDataFormUpdate(data) {
+        this.#codeUpdate.value = data.code;
+        this.#nameUpdate.value = data.name;
+        this.#dateOfBirthUpdate.value = data.dateOfBirth;
+        this.#classCodeUpdate.value = data.classCode;
+        this.#genderUpdate.value = data.gender;
+    }
+
+    #addEventIconUpdate() {
+        this.#icons.forEach((icon) => {
+            icon.onclick = (e) => {
+                const input = e.target.parentElement.querySelector('input');
+                input.disabled = !input.disabled;
+                if (!input.disabled) {
+                    input.style.backgroundColor = '#fff';
+                    input.focus();
+                } else {
+                    input.style.backgroundColor = 'transparent';
+                }
+            };
+        });
+    }
+
+    async #handleUpdate(id) {
+        const student = new Student(
+            this.#codeUpdate.value.trim(),
+            this.#nameUpdate.value.trim(),
+            this.#genderUpdate.value,
+            this.#dateOfBirthUpdate.value,
+            this.#classCodeUpdate.value,
+            this.#image
+        );
+
+        const respone = await this.#controler.handleUpdateStudent(id, student);
+        switch (respone.type) {
+            case 'success': {
+                alert(respone.message);
+                break;
+            }
+
+            default:
+                this.#addDataFormUpdate(respone.data);
+                break;
+        }
+    }
+
+    #addEventButtonUpdate(id) {
+        this.#btnUpdate.onclick = (e) => {
+            this.#handleUpdate(id);
+        };
+    }
+
     async #handleViewProfile(id) {
         const { isError, message, data } = await this.#controler.getProfile(id);
         if (isError) {
@@ -47,6 +124,9 @@ export default class StudentItemView {
         } else {
             if (data) {
                 this.#handleActionOverlay('block', 'block');
+                this.#addDataFormUpdate(data);
+                this.#addEventIconUpdate();
+                this.#addEventButtonUpdate(data.id);
             } else {
                 alert('please wait');
             }
