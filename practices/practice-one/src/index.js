07c6c92c32261908f45247d595dafe5006f49b1d
duong <phamtanduongtk29@gmail.com>
2022-11-04 16:17:59 +0700

    #19 Add event DOMContentLoaded to src/index.js

--- practices/practice-one/src/index.js
+++ practices/practice-one/src/index.js
@@ -1,6 +1,12 @@
 import App from './scripts/views/app.view.js';
 
-document.addEventListener('DOMContentLoaded', (e) => {
+function start() {
     const app = new App();
     app.init();
-});
+}
+
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', start);
+} else {
+    start();
+}
